83f256ee52b884d2fcd1699778c120f4943cd793
udaykumardl <udaykumardl744@gmail.com>
2024-10-02 18:26:38 +0530

    Subscribing and Dispatching Actions

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -3,7 +3,7 @@ import {  useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
   const dispatch=useDispatch();
-  const counter=useSelector(state=>state.counter)
+  const counter = useSelector(state=>state.counter);
 
   const incrementHandler=()=>{
     dispatch({type:'increment'});
@@ -16,8 +16,8 @@ const Counter = () => {
 
   return (
     <main className={classes.counter}>
-      <h1>Redux Counter</h1>
       
+      <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
